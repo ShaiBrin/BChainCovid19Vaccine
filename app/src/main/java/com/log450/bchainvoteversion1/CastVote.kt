88dd37f7e8012deda1ade8d94c327f198d6ac56d
Ishraq Shabab <Ishr_Shai@live.ca>
2021-03-29 20:26:53 -0400

    Blocks can be added to the blockchain.

--- app/src/main/java/com/log450/bchainvoteversion1/CastVote.kt
+++ app/src/main/java/com/log450/bchainvoteversion1/CastVote.kt
@@ -3,9 +3,14 @@ package com.log450.bchainvoteversion1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import com.log450.bchainvoteversion1.Model.Block
 import com.log450.bchainvoteversion1.R
+import com.log450.bchainvoteversion1.Repository.Blockchain
+import com.log450.bchainvoteversion1.Repository.Blockchain.getPreviousDoc
+import com.log450.bchainvoteversion1.Repository.Blockchain.getPreviousHash
+import com.log450.bchainvoteversion1.Repository.Blockchain.updateReference
 import com.log450.bchainvoteversion1.Utils.Constants.EXTRA_STUDENT
 
 class CastVote : AppCompatActivity() {
@@ -16,8 +21,12 @@ class CastVote : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cast_vote)
         val value = intent.getStringExtra(EXTRA_STUDENT) as String
-        /*block  =  Block("", value);
-        fbase.collection("blockchain2").document("1").set(block)
-        Log.d("Block", block.getHash());*/
+        block = Block(getPreviousHash(), value)
+        fbase.collection("blockchain").document((getPreviousDoc().toInt()+1).toString()).set(block)
+        updateReference(block)
+
     }
+
+
+
 }
